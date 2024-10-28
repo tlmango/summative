3d4c79443aa333bcf35d3c96d63d6b7dd91df6b1
t_mango <126527526+tlmango@users.noreply.github.com>
2024-10-28 09:45:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => { 
+  const collapsible = document.querySelector(".collapsible"); 
   const content = document.querySelector(".content"); 
   const sidebar = document.getElementById("sidebar"); 
   
