613427c53fcd51179ca07be43c440a8b6f81ea63
t_mango <126527526+tlmango@users.noreply.github.com>
2024-10-28 09:20:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => { 
   const collapsible = document.querySelector(".collapsible"); 
   const content = document.querySelector(".content"); 
+  const sidebar = document.getElementById("sidebar"); 
   
   collapsible.addEventListener("click", () => { 
     content.style.display = content.style.display === "block" ? "none" : "block"; 
+    sidebar.classList.toggle("collapsed");   
   }); 
 });
+
+
