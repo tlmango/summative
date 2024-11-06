bf3654212131d4feacdf0feade4bdb7afe92f267
t_mango <126527526+tlmango@users.noreply.github.com>
2024-11-05 20:16:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
   }); 
 });
 
+function closepreloader(){
+            document.getElementById("preloader").style.display = 'none';
+        }
+        window.addEventListener("load",function(){
+            setTimeout(closepreloader, 2000);
+            
+        });
