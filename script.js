9446d17167788485d7006af81a695780e3b4197d
t_mango <126527526+tlmango@users.noreply.github.com>
2024-11-05 22:28:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 $(window).on('load',function(){
-  setTimeout(function(){ // allowing 3 secs to fade out loader
+  setTimeout(function(){
   $('.page-loader').fadeOut('slow');
   },3500);
 });
