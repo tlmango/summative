77e79e83fd3e8d6dccabbfe7ac7ac2547686215a
t_mango <126527526+tlmango@users.noreply.github.com>
2024-11-05 22:27:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }); 
 });
 
-function closepreloader(){
-            document.getElementById("preloader").style.display = 'none';
-        }
-        window.addEventListener("load",function(){
-            setTimeout(closepreloader, 2000);
-            
-        });
+$(window).on('load',function(){
+  setTimeout(function(){ // allowing 3 secs to fade out loader
+  $('.page-loader').fadeOut('slow');
+  },3500);
+});
